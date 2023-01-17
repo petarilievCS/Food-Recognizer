3a9f6f5e3508f9c544c428585f501473f7b32e23
Petar Iliev <petariliev2002@gmail.com>
2023-01-17 13:33:27 -0600

    Implement camera view

--- Food-Recognizer/Controllers/HomeViewControlelr.swift
+++ Food-Recognizer/Controllers/HomeViewControlelr.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import AVFoundation
 
-class HomeViewControlelr: UIViewController {
-
+class HomeViewControlelr: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    var camera : UIImagePickerController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        camera = UIImagePickerController()
+        camera?.delegate = self
+        camera?.sourceType = .camera
     }
-
-
+    
+    // Present camera session when button pressed
+    @IBAction func scanButtonPressed(_ sender: UIButton) {
+        
+        // Request camera access
+        AVCaptureDevice.requestAccess(for: AVMediaType.video, completionHandler: { permissionGranted in
+            if permissionGranted {
+                self.presentCamera()
+            }
+        })
+        
+    }
+    
+    // Create a camera session and present it to user
+    func presentCamera() {
+        guard let safeCamera = camera else {
+            fatalError("Camera picker not initialized")
+        }
+        DispatchQueue.main.async {
+            self.present(safeCamera, animated: true)
+        }
+    }
+    
 }
 
