678733a1c347de1d60ff5adf11ebc361eff581f3
Petar Iliev <petariliev2002@gmail.com>
2023-01-18 09:36:19 -0600

    Start building API request functionality

--- Food-Recognizer/Model/APIManager.swift
+++ Food-Recognizer/Model/APIManager.swift
@@ -9,4 +9,16 @@ import Foundation
 
 struct APIManager {
     
+    let URLString = "https://api.nal.usda.gov/fdc/v1/search"
+    
+    func performRequest(for food: String) {
+        let URL = URL(string: "\(URLString)&api_key=\(Keys.foodDataCentralAPIKey)&query=\(food)&pageSize=1&pageNumber=1&dataType=Foundation")
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: URL) { data, response, error in
+            <#code#>
+        }
+    }
+    
 }
+
+
